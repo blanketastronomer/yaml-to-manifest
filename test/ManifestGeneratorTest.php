a7a36d61a49bd4950dc3b491f440e02142308838
blanketastronomer <blanketastronomer@users.noreply.github.com>
2015-05-25 01:41:23 -0700

    Add ManifestGenerator test class

---
+++ test/ManifestGeneratorTest.php
@@ -0,0 +1,14 @@
+<?php
+
+require 'src/ManifestGenerator.php';
+
+class ManifestGeneratorTest extends PHPUnit_Framework_TestCase
+{
+    protected $generator;
+    protected $manifest_section;
+
+    protected function setUp()
+    {
+        // TODO: Set up tests
+    }
+}
