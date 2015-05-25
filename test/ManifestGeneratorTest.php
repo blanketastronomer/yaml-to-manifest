c2eeeb9788d5273c30de8c567f7a785ca629d151
blanketastronomer <blanketastronomer@users.noreply.github.com>
2015-05-25 01:46:04 -0700

    Add 'addSection()' test

--- test/ManifestGeneratorTest.php
+++ test/ManifestGeneratorTest.php
@@ -9,6 +9,38 @@ class ManifestGeneratorTest extends PHPUnit_Framework_TestCase
 
     protected function setUp()
     {
-        // TODO: Set up tests
+        $this->generator = new ManifestGenerator('test/manifest.yml');
+        $this->manifest_section = <<<'MANIFEST'
+$manifest = array (
+  'author' => '<author_name>',
+  'description' => '<module_description>',
+  'name' => '<module_name>',
+  'acceptable_sugar_versions' => 
+  array (
+    0 => '6.*',
+  ),
+  'acceptable_sugar_flavors' => 
+  array (
+    0 => 'CE',
+    1 => 'PRO',
+    2 => 'CORP',
+    3 => 'ENT',
+    4 => 'ULT',
+  ),
+  'key' => '<module_key>',
+  'type' => 'module',
+  'icon' => '<icon_file>',
+  'is_uninstallable' => true,
+  'published_date' => '@@PUBLISH_DATE@@',
+  'readme' => '<path_to_readme>',
+  'version' => '@@VERSION@@',
+);
+MANIFEST;
+    }
+
+    public function testAddsManifestSection()
+    {
+        $this->assertEquals($this->manifest_section, $this->generator->addSection('manifest'));
     }
 }
+
